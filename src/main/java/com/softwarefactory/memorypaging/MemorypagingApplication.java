d2688c994ceb972158a0baad74d7b497d3a82197
Daniela Lana <danioclana@gmail.com>
2023-12-10 01:05:17 -0300

    Commit inicial

---
+++ src/main/java/com/softwarefactory/memorypaging/MemorypagingApplication.java
@@ -0,0 +1,13 @@
+package com.softwarefactory.memorypaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorypagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorypagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarefactory/memorypaging/MemorypagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefactory.memorypaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorypagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
