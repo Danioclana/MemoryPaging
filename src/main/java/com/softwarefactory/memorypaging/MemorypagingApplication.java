09a1b4d0ecffc5554b763232f3c08c9bdc7989b8
Daniela Lana <danioclana@gmail.com>
2023-12-10 22:52:05 -0300

    Inserindo BD

--- src/main/java/com/softwarefactory/memorypaging/MemorypagingApplication.java
+++ src/main/java/com/softwarefactory/memorypaging/MemorypagingApplication.java
@@ -6,8 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MemorypagingApplication {
 
+	//mvn spring-boot:run
 	public static void main(String[] args) {
 		SpringApplication.run(MemorypagingApplication.class, args);
+		try {
+			System.out.println("\n\n\033[0;32m" + "http://localhost:8080/" + "\033[0m");
+		} catch (Exception e) {
+			System.out.println("\033[0;31m" + "Erro ao iniciar o backend" + "\033[0m");
+			System.out.println(e);
+		}
 	}
 
 }
