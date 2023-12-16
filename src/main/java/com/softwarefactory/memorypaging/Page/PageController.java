e68424be0a59b15b261fb0f00c0bc7d3dacc3019
Daniela Lana <danioclana@gmail.com>
2023-12-16 00:21:49 -0300

    Corrigindo bugs

--- src/main/java/com/softwarefactory/memorypaging/Page/PageController.java
+++ src/main/java/com/softwarefactory/memorypaging/Page/PageController.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Getter;
@@ -21,8 +21,8 @@ public class PageController {
 
     ArrayList<Page> pages = new ArrayList<Page>();
 
-    @PostMapping("/createAll")
-    public ResponseEntity<?> createPagesRandom(@RequestBody int numberOfPages) {
+    @GetMapping("/createAll")
+    public ResponseEntity<?> createPagesRandom(@RequestParam(required = false) Integer numberOfPages) {
 
         for (int i = 0; i < numberOfPages; i++) {
 
--- src/main/java/com/softwarefactory/memorypaging/RAM/FrameController.java
+++ src/main/java/com/softwarefactory/memorypaging/RAM/FrameController.java
@@ -6,8 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Getter;
@@ -32,15 +32,15 @@ public class FrameController {
         return ResponseEntity.status(201).body("Frame " + frames.size() + " created successfully");
     }
 
-    @PostMapping("/createAll")
-    public ResponseEntity<?> createFrames(@RequestBody int numberOfFrames) {
+    @GetMapping("/createAll")
+    public ResponseEntity<?> createFrames(@RequestParam(required = false) Integer numberOfFrames) {
 
-        for (Frame newFrame : frames) {
-            newFrame = new Frame();
-            newFrame.setId(frames.size() + 1);
-            newFrame.setPage(null);
+        for (Frame frame : frames) {
+            frame = new Frame();
+            frame.setId(frames.size() + 1);
+            frame.setPage(null);
 
-            frames.add(newFrame);
+            frames.add(frame);
         }
         
         return ResponseEntity.status(201).body(frames.size() + " frames created successfully");
