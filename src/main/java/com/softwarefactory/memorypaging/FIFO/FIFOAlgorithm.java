0c72fef85ff8c52165cb4bab48cd1d6941a7e38d
Vinicius Marques <viniciusmrqs@outlook.com>
2023-12-19 20:25:20 -0300

    ajuste

--- src/main/java/com/softwarefactory/memorypaging/FIFO/FIFOAlgorithm.java
+++ src/main/java/com/softwarefactory/memorypaging/FIFO/FIFOAlgorithm.java
@@ -21,13 +21,14 @@ public class FIFOAlgorithm {
     private List<Frame> frames = new ArrayList<>();
     private List<Page> pages = new ArrayList<>();
 
+    private int[] pagesIdList;
 
     @PostMapping("/acessPages")
     public ResponseEntity<List<Object>> FIFO_acessPages(@RequestBody int[] pagesId) {
         List<Object> response = new ArrayList<>();
 
         FIFO_init();
-        
+
         for (int pageId : pagesId) {
             response.add(FIFO_acessPage(pageId));
         }
@@ -57,6 +58,8 @@ public class FIFOAlgorithm {
         frames.addAll(FrameController.frames);
         pages.addAll(PageController.pages);
 
+        pagesIdList = new int[frames.size()];
+
         contPageFaults = 0;
     }
 
@@ -64,7 +67,9 @@ public class FIFOAlgorithm {
         Page page = pageController.isExists(pageId);
 
         if (page == null) {
-            return ResponseEntity.status(404).body("Page " + pageId + " not found");
+            System.out.println("PÁGINA " + pageId + " NÃO EXISTE NA MEMÓRIA");
+
+            return null;
         }
 
         int frameIndex = frameController.findPage(pageId);
@@ -75,8 +80,14 @@ public class FIFOAlgorithm {
 
             System.out.println("PÁGINA " + pageId + " JÁ ESTÁ NO QUADRO " + frameIndex);
 
-            return responseConstructor(pageId, frameController.findPage(pageId), pageFaultHistoric,
-            "PÁGINA " + pageId + " JÁ ESTÁ NO QUADRO " + frameController.findPage(pageId));
+            List<Integer> quadros = new ArrayList<>();
+
+            for (int j = 0; j < pagesIdList.length; j++) {
+                quadros.add(pagesIdList[j]);
+            }
+
+            return responseConstructor(pageId, quadros, pageFaultHistoric,
+                    "PÁGINA " + pageId + " JÁ ESTÁ NO QUADRO " + frameController.findPage(pageId));
         }
 
         for (Frame frame : frames) {
@@ -88,17 +99,23 @@ public class FIFOAlgorithm {
 
                 ageIncrement();
 
-                System.out.println("PÁGINA " + pageId + " CARREGADA NO QUADRO " + frame.getId()
-                );
-                return responseConstructor(pageId, frame.getId(), pageFaultHistoric,
-                                "PÁGINA " + pageId + " CARREGADA NO QUADRO " + frame.getId());
+                FIFO_pagesIdListUpdate(frame.getId(), page.getId());
+
+                List<Integer> quadros = new ArrayList<>();
+                for (int j = 0; j < pagesIdList.length; j++) {
+                    quadros.add(pagesIdList[j]);
+                }
+
+                System.out.println("PÁGINA " + pageId + " CARREGADA NO QUADRO " + frame.getId());
+                return responseConstructor(pageId, quadros, pageFaultHistoric,
+                        "PÁGINA " + pageId + " CARREGADA NO QUADRO " + frame.getId());
             }
         }
 
         Page pageToRemove = frames.get(0).getPage();
 
         for (Frame frame : frames) {
-            
+
             if (frame.getPage().getAge() > pageToRemove.getAge()) {
                 pageToRemove = frame.getPage();
             }
@@ -114,13 +131,21 @@ public class FIFOAlgorithm {
 
         ageIncrement();
 
+        FIFO_pagesIdListUpdate(frame.getId(), page.getId());
+
+        List<Integer> quadros = new ArrayList<>();
+        for (int j = 0; j < pagesIdList.length; j++) {
+
+            quadros.add(pagesIdList[j]);
+        }
+
         System.out.println("PÁGINA " + pageId + " SUBSTITUI " + pageToRemove.getId() + " NO QUADRO " + frame.getId());
 
-        return responseConstructor(pageId, frame.getId(), pageFaultHistoric,
+        return responseConstructor(pageId, quadros, pageFaultHistoric,
                 "PÁGINA " + pageId + " SUBSTITUI " + pageToRemove.getId() + " NO QUADRO " + frame.getId());
     }
 
-    private Object responseConstructor(int pageId, int frameId, boolean pageFaultHistoric, String action) {
+    private Object responseConstructor(int pageId, Object frameId, boolean pageFaultHistoric, String action) {
         Object[] response = new Object[4];
 
         response[0] = pageId;
@@ -129,14 +154,23 @@ public class FIFOAlgorithm {
         response[3] = action;
 
         return response;
-    } 
+    }
+
+    private void FIFO_pagesIdListUpdate(int frameId, int pageId) {
+        for (int i = 0; i < frames.size(); i++) {
+            if ((i + 1) == frameId) {
+                pagesIdList[i] = pageId;
+                return;
+            }
+        }
+    }
 
     private void ageIncrement() {
         frames.stream().filter(frame -> frame.getPage() != null)
                 .forEach(frame -> frame.getPage().setAge(frame.getPage().getAge() + 1));
     }
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         FIFOAlgorithm fifo = new FIFOAlgorithm();
 
         PageController pageController = new PageController();
@@ -144,10 +178,11 @@ public class FIFOAlgorithm {
 
         FrameController frameController = new FrameController();
         frameController.createFrames(3);
-        
-        fifo.FIFO_acessPages(new int[] { 8, 1, 2, 3, 1, 4, 1, 5, 3, 4, 1, 4, 3, 2, 3, 1, 2, 8, 1, 2 });
+
+        fifo.FIFO_acessPages(new int[] { 8, 1, 2, 3, 1, 4, 1, 5, 3, 4, 1, 4, 3, 2, 3,
+                1, 2, 8, 1, 2 });
 
         System.out.println("ContPageFaults: " + fifo.contPageFaults);
-    }*/
-    
+    }
+
 }
